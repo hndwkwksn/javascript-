ad65ca20d7f285d5294c4192746f938e26cd6d3f
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-12 12:57:54 +0000

    [Finish]2章/練習問題

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ let str2 = 'World!!';
 alert(str1 + str2);
 
 let i;
-let num = 0;
+let num = 1;
 
 for(i = 1; i < 11; i++){
   num = num + i;
 }
 
 alert('1から10まで足し算した結果は' + num + 'です');
+
+let a = 62;
+let b = 2;
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a / b;
+alert(`${a} + ${b} =` + c);
+
+alert(`${a} - ${b} =` + d);
+
+alert(`${a} * ${b} =` + e);
+
+alert(`${a} / ${b} =` + f);
