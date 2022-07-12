b062d8eeb906e66971badcc635332f3083ed65ce
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-12 12:13:33 +0000

    [Add]app.js/while文

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 let str1 = 'Hello';
 let str2 = 'World!!';
 alert(str1 + str2);
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
